d4bc2f090fdd985d4f46d101ceee2f7f9583d7f5
Joriom <jakub.mroczkowski@gmail.com>
2018-06-15 18:59:09 +0200

    Updated cherry-picked commit for FOV mod.

--- applyFOVmod.sh
+++ applyFOVmod.sh
@@ -4,5 +4,5 @@ cd ../OneLife
 
 git remote add fovmod "https://github.com/Joriom/OneLifeXcompile.git"
 git fetch fovmod
-git cherry-pick 58a22df7658fc49269dc
+git cherry-pick f4227825f0731cfe5ec1bdae0bbafb84d0194965
 git mergetool
