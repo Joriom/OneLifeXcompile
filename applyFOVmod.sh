fedbebfad806ac6ad1cd2f3f33b60d4644367928
Joriom <jakub.mroczkowski@gmail.com>
2018-06-15 18:56:52 +0200

    Fixed error with wrong commits being pulled by FOV aplicator.

--- applyFOVmod.sh
+++ applyFOVmod.sh
@@ -4,5 +4,5 @@ cd ../OneLife
 
 git remote add fovmod "https://github.com/Joriom/OneLifeXcompile.git"
 git fetch fovmod
-git cherry-pick fovmod/master 58a22df7658fc49269dc
+git cherry-pick 58a22df7658fc49269dc
 git mergetool
